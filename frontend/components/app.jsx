53d4f9015027c11b28197fef4ec66f460fd63ead
Kelvin Cho <cho.k.kelvin@gmail.com>
2018-06-15 17:42:32 -0700

    Started frontend auth

---
+++ frontend/components/app.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Route, Redirect, Switch, Link } from 'react-router-dom';
+import { AuthRoute, ProtectedRoute } from '../utils/route';
+import Wrapper from './loader/wrapper';
+import Loadable from 'react-loadable';
+
+const NavigationContainer = Wrapper({
+	loader: () => import('./navigation/navigation_container')
+});
+
+const Home = Wrapper({
+	loader: () => import('./home/home')
+});
+
+const SignupContainer = Wrapper({
+	loader: () => import('./session/signup_container')
+});
+
+const LoginContainer = Wrapper({
+	loader: () => import('./session/login_container')
+});
+
+Loadable.preloadAll();
+
+const App = () => (
+	<div id="app">
+		<header>
+			<Route path="/" component={NavigationContainer} />
+		</header>
+		<Switch>
+			<Route exact path="/" component={Home} />
+			<AuthRoute path="/signup" component={SignupContainer} />
+			<AuthRoute path="/login" component={LoginContainer} />
+			<Redirect to="/" />
+		</Switch>
+		<footer />
+	</div>
+);
+
+export default App;
